8407ad777c0e4d56bb76a551a74893f5755a42f3
Derek Graham <deejaygraham@gmail.com>
2017-11-30 13:16:36 +0000

    Add more commands to example

--- scripts/chat-hello.py
+++ scripts/chat-hello.py
@@ -6,4 +6,23 @@ time.sleep(2)
 
 # Connect to minecraft by creating the minecraft object
 world = minecraft.Minecraft.create()
-world.postToChat("Hello, Minecraft World")
+count = 0
+
+x = 0
+y = 0
+z = 0
+
+while True:
+    count += 1
+
+    block = world.getBlock(x, y, z)
+
+    print("block = " + str(block))
+
+    world.saveCheckpoint()
+    world.postToChat("Hello, Minecraft World " + str(count))
+    time.sleep(1)
+    world.restoreCheckpoint()
+
+    block += 1
+    world.setBlock(x, y, z, block)
